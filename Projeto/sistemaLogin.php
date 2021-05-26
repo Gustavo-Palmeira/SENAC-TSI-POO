a62a5af93b5ee7f294eb05ae18afb92316ba1042
Gustavo Palmeira <gustavo_palmeira07@hotmail.com>
2021-05-25 22:10:44 -0300

    Main att

--- Projeto/sistemaLogin.php
+++ Projeto/sistemaLogin.php
@@ -19,40 +19,46 @@ class Main {
     }
 
     public function checkOperation() {
-        if (isset($_POST['startLogin'])) {
-            if ($this->objLogin->setLogin()) {
-                $this->objLogin->userLogin();
-            };
+        if (key($_POST)) {
+            $functionName = array_key_last($_POST);
+            $this->$functionName();
         }
-        if (isset($_POST['register'])) {
-            if ($this->objRegister->setRegister()) {
-                $erros = $this->consistencyCheck($this->objRegister);
-                if (count($erros) > 0) {
-                    header("Location: ./public/html/create-user.php?msg=" . urlencode(serialize($erros)));
-                } else {
-                    $this->objRegister->registerUser();
-                    header("Location: ./index.php");
-                }
-            };
-        }
-        if (isset($_POST['changePasswordForm'])) {
-            if ($this->objChangePassword->setPassword()) {
-                $erros = $this->consistencyCheck($this->objChangePassword);
-                if (count($erros) > 0) {
-                    header("Location: ./public/html/change-password.php?msg=" . urlencode(serialize($erros)));
-                } else {
-                    $this->objChangePassword->changePassword();
-                    header("Location: ./index.php");
-                }
-            };
-        }
+    }
+
+    private function startLogin() {
+        if ($this->objLogin->setLogin()) {
+            $this->objLogin->userLogin();
+        };
+    }
+
+    private function register() {
+        if ($this->objRegister->setRegister()) {
+            $erros = $this->consistencyCheck($this->objRegister);
+            if (count($erros) > 0) {
+                header("Location: ./public/html/create-user.php?msg=" . urlencode(serialize($erros)));
+            } else {
+                $this->objRegister->registerUser();
+                header("Location: ./index.php");
+            }
+        };
+    }
+
+    private function changePasswordForm() {
+        if ($this->objChangePassword->setPassword()) {
+            $erros = $this->consistencyCheck($this->objChangePassword);
+            if (count($erros) > 0) {
+                header("Location: ./public/html/change-password.php?msg=" . urlencode(serialize($erros)));
+            } else {
+                $this->objChangePassword->changePassword();
+                header("Location: ./index.php");
+            }
+        };
     }
 
     public function consistencyCheck(object $object) {
         $consistencyTest = new CheckConsistency;
-
         $erros = $consistencyTest->check($object);
-        
+
         return $erros;
     }
 }
